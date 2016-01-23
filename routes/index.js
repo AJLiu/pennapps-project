d96014876e49e74abb3d7e7cf18c99b482b66395
Ivon Liu <ivon.liu2011@gmail.com>
2016-01-23 01:23:35 -0500

    Add mongoose stuff to index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Competition = mongoose.model('Competition');
+var School = mongoose.model('School');
+var Submission = mongoose.model('Submission');
+var User = mongoose.model('User');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
