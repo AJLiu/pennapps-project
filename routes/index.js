c597ce529789a4fa8181cce5c44bed58aaeca643
Anthony Liu <Anthony Liu>
2016-01-23 12:16:55 -0500

    move stuff into users route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var jwt = require('express-jwt');
-var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+var auth = jwt({
+    secret: 'SECRET',
+    userProperty: 'payload'
+});
 
 var mongoose = require('mongoose');
 var Competition = mongoose.model('Competition');
@@ -20,55 +23,6 @@ router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'Express'
     });
-});
-
-router.post('/register', function(req, res, next) {
-  if(!req.body.email || !req.body.password) {
-    return res.status(400).json({
-      message: 'Please fill out all fields'
-    });
-  }
-
-  var user = new User();
-
-  user.first_name = req.body.first_name;
-  user.last_name = req.body.last_name;
-  user.email = req.body.email;
-  user.grade = req.body.grade;
-
-  user.setPassword(req.body.password);
-
-  user.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-
-    return res.json({
-      token: user.generateJWT()
-    });
-  });
-});
-
-router.post('/login', function(req, res, next) {
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).json({
-      message: 'Please fill out all fields'
-    });
-  }
-
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
-
-    if (user) {
-      return res.json({
-        token: user.generateJWT()
-      });
-    } else {
-      return res.status(401).json(info);
-    }
-  })(req, res, next);
 });
 
 module.exports = router;
