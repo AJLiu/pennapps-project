f3e08b171eebf40406a3ac2a5d0ceb82d4cd2eff
Alan Chen <alancxchen@gmail.com>
2016-01-24 09:04:47 -0500

    cancel button

--- public/js/controllers/competitionRegisteredCtrl.js
+++ public/js/controllers/competitionRegisteredCtrl.js
@@ -4,6 +4,18 @@ app.controller('CompetitionRegisteredCtrl', [
   '$state',
   'auth',
   '$http',
-  function($scope, $rootScope, $state, auth, $http) {
+  'competition',
+  function($scope, $rootScope, $state, auth, $http, competition) {
+  	$scope.competition = competition;
+  	$scope.unregister = function(){
+	  $http.post('/competitions/'+competition._id+'/withdraw', null, {
+      	headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).then(function(response) {
+      	console.log(JSON.stringify(response.data));
+      	$state.go('dash');
+      }, function(error) {
+      	console.log(error);
+      });
+    };
   }
 ]);
--- public/js/controllers/competitionUnregisteredCtrl.js
+++ public/js/controllers/competitionUnregisteredCtrl.js
@@ -4,7 +4,20 @@ app.controller('CompetitionUnregisteredCtrl', [
   '$state',
   'auth',
   '$http',
-  function($scope, $rootScope, $state, auth, $http) {
-    
+  'competition',
+  '$stateParams',
+  function($scope, $rootScope, $state, auth, $http, competition, $stateParams) {
+  	$scope.competition = competition;
+
+  	$scope.redirect() = function(){
+      $http.post('/competitions/'+competition._id+'/register', null, {
+      	headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).then(function(response) {
+      	console.log(JSON.stringify(response.data));
+      	$state.go('dash');
+      }, function(error) {
+      	console.log(error);
+      });
+    };
   }
 ]);
