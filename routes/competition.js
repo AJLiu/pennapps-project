97947a8fb2c5ef5e4c14f117b3d70b05c194e0ff
Anthony Liu <Anthony Liu>
2016-01-24 03:57:15 -0500

    updated competition routes

--- routes/competition.js
+++ routes/competition.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var passport = require('passport');
 var jwt = require('express-jwt');
 var auth = jwt({
-    secret: 'SECRET',
-    userProperty: 'payload'
+	secret: 'SECRET',
+	userProperty: 'payload'
 });
 
 var mongoose = require('mongoose');
@@ -15,95 +15,100 @@ var User = mongoose.model('User');
 
 router.get('/', function(req, res, next) {
 
-    Competition.find(function(err, doc) {
-        if (err) {
-            console.error(err);
-            res.status(400).send(err);
-        }
-        var date = new Date();
-        for(var i in doc) {
-            if(date < doc[i].start_date)
-                doc[i].prompt="Come back later";
-            else if(date > doc[i].end_date)
-                doc[i].prompt+="\nNOTE: THIS EVENT IS OVER.";
-        }
-        res.json(doc);
-    });
+	Competition.find(function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		var date = new Date();
+		for (var i in doc) {
+			if (date < doc[i].start_date)
+				doc[i].prompt = "Come back later";
+			else if (date > doc[i].end_date)
+				doc[i].prompt += "\nNOTE: THIS EVENT IS OVER.";
+		}
+		res.json(doc);
+	});
 });
 
 router.get('/:id', function(req, res, next) {
-    Competition.findOne({
-        _id: req.param('id')
-    }, function(err, doc) {
-        if (err) {
-            console.error(err);
-            res.status(400).send(err);
-        }
-        var date = new Date();
-        if(date < doc.start_date)
-            doc.prompt="Come back later";
-        else if(date > doc.end_date)
-            doc.prompt+="\nNOTE: THIS EVENT IS OVER.";
-        res.json(doc);
-    });
+	Competition.findOne({
+		_id: req.param('id')
+	}, function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		var date = new Date();
+		if (date < doc.start_date)
+			doc.prompt = "Come back later";
+		else if (date > doc.end_date)
+			doc.prompt += "\nNOTE: THIS EVENT IS OVER.";
+		res.json(doc);
+	});
 });
 
 router.post('/', function(req, res, next) {
-    Competition.create({
-        name: req.body.name,
-        start_date: req.body.start_date,
-        end_date: req.body.end_date,
-        prompt: req.body.prompt,
-        students: req.body.students,
-        submissions: req.body.submissions
-    }, function(err, doc) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        } else {
-            res.send(doc);
-        }
-    });
+	Competition.create({
+		name: req.body.name,
+		start_date: req.body.start_date,
+		end_date: req.body.end_date,
+		prompt: req.body.prompt,
+		students: req.body.students,
+		submissions: req.body.submissions,
+		image: req.body.image
+	}, function(err, doc) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		} else {
+			res.send(doc);
+		}
+	});
 });
 
 router.delete('/:id', function(req, res, next) {
-    Competition.findByIdAndRemove(req.param('id'), function(err, doc) {
-        if (err) {
-            console.error(err);
-            res.status(400).send(err);
-        }
-        res.json(doc);
-    });
+	Competition.findByIdAndRemove(req.param('id'), function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		res.json(doc);
+	});
 });
 
 router.patch('/:id', function(req, res, next) {
-    var update = {};
-    if (req.body.name)
-        update.name = req.body.name;
-    if (req.body.start_date)
-        update.start_date = req.body.start_date;
-    if (req.body.end_date)
-        update.end_date = req.body.end_date;
-    if (req.body.prompt)
-        update.prompt = req.body.prompt;
-    if (req.body.students)
-        update.students = req.body.students;
-    if (req.body.submissions)
-        update.submissions = req.body.submissions;
+	var update = {};
+	if (req.body.name)
+		update.name = req.body.name;
+	if (req.body.start_date)
+		update.start_date = req.body.start_date;
+	if (req.body.end_date)
+		update.end_date = req.body.end_date;
+	if (req.body.prompt)
+		update.prompt = req.body.prompt;
+	if (req.body.students)
+		update.students = req.body.students;
+	if (req.body.submissions)
+		update.submissions = req.body.submissions;
+	if (req.body.image)
+		update.image = req.body.image;
 
-    Competition.findOneAndUpdate({
-        _id: req.param('id')
-    }, update, function(err, doc) {
-        if(err){
-            console.error(err);
-            res.status(400).send(err);
-        }
-        res.send(doc);
-    });
+	Competition.findOneAndUpdate({
+		_id: req.param('id')
+	}, update, function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		res.send(doc);
+	});
 });
 
 router.param('id', function(req, res, next, id) {
-	Competition.findOne({_id: id}).then(function(competition) {
+	Competition.findOne({
+		_id: id
+	}).then(function(competition) {
 		req.competition = competition;
 		return next();
 	}, function(error) {
@@ -112,35 +117,105 @@ router.param('id', function(req, res, next, id) {
 });
 
 router.post('/:id/register', auth, function(req, res, next) {
-	Competition.update(
-		{_id: req.competition._id},
-		{$push: {students: req.payload._id}}
-	).then(function() {
-		return User.update(
-      {_id: req.payload._id},
-      {$push: {competitions: req.competition._id}}
-    );
+	Competition.update({
+		_id: req.competition._id
+	}, {
+		$push: {
+			students: req.payload._id
+		}
 	}).then(function() {
-    res.send(req.competition);
-  }, function(error) {
-    res.status(400).send(error);
-  });
+		return User.update({
+			_id: req.payload._id
+		}, {
+			$push: {
+				competitions: req.competition._id
+			}
+		});
+	}).then(function() {
+		res.send(req.competition);
+	}, function(error) {
+		res.status(400).send(error);
+	});
 });
 
 router.post('/:id/withdraw', auth, function(req, res, next) {
-  Competition.update(
-    {_id: req.competition._id},
-    {$pull: {students: req.payload._id}}
-  ).then(function() {
-    return User.update(
-      {_id: req.payload._id},
-      {$pull: {competitions: req.competition._id}}
-    ).then(function() {
-      res.send(req.competition);
-    }, function(error) {
-      res.status(400).send(error);
-    });
-  });
+	Competition.update({
+		_id: req.competition._id
+	}, {
+		$pull: {
+			students: req.payload._id
+		}
+	}).then(function() {
+		return User.update({
+			_id: req.payload._id
+		}, {
+			$pull: {
+				competitions: req.competition._id
+			}
+		}).then(function() {
+			res.send(req.competition);
+		}, function(error) {
+			res.status(400).send(error);
+		});
+	});
+});
+
+router.get('/current', function(req, res, next) {
+	var date = new Date();
+
+	Competition.find({
+		start_date: {
+			$lt: date
+		},
+		end_date: {
+			$gt: date
+		}
+	}, function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		res.json(doc);
+	});
+});
+
+
+router.get('/past', function(req, res, next) {
+	var date = new Date();
+
+	Competition.find({
+		end_date: {
+			$lt: date
+		}
+	}, function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		for (var i=0; i<competitions.length; i++) {
+	      competitions[i].prompt+="\nNOTE: THIS EVENT IS OVER.";
+	    }
+		res.json(doc);
+	});
+});
+
+router.get('/upcoming', function(req, res, next) {
+	var date = new Date();
+
+	Competition.find({
+		start_date: {
+			$gt: date
+		}
+	}, function(err, doc) {
+		if (err) {
+			console.error(err);
+			res.status(400).send(err);
+		}
+		for (var i = 0; i < competitions.length; i++) {
+			competitions[i].prompt = "Come back later";
+		}
+		res.json(doc);
+	});
 });
 
 module.exports = router;
